7badca67fd43c0ae570b951da1d22e761f006392
Motasem  Almohtadi <malmohtadi@deloitte.com>
2022-01-15 21:46:37 +0200

    First commit

---
+++ src/interfaces/hompage.ts
@@ -0,0 +1,45 @@
+import { ICustomFields } from './custom-fields';
+
+export interface IBanner {
+    id: number;
+    imageUrl: string;
+    isClickable: boolean;
+    isOfferTab: boolean;
+    categoryId: number;
+    subCategoryId: number;
+    order: number;
+}
+export interface ICategory {
+    id: number,
+    imageUrl: string,
+    isRetail: boolean,
+    isWholeSale: boolean,
+    name: string,
+    order: number,
+    subCategories: ISubCategory[]
+}
+export interface ISubCategory {
+    id: number,
+    isRetail: boolean,
+    isWholeSale: boolean,
+    name: string,
+    order: number
+}
+export interface IAdminSettingsResponse {
+    chooseDeliveryEnabled: boolean,
+    choose_delivery_period_enabled?: boolean,
+    deliveryInSameDayEnabled: boolean,
+    id: number,
+    maxTotalPriceRetail: number,
+    maxTotalPriceWholeSale: number,
+    minTotalPriceRetail: number,
+    minTotalPriceWholeSale: number,
+    userId: number,
+    visaOnDeliveryEnabled: boolean,
+    wholeSaleEnabled: boolean
+}
+export interface IHomePageResponse {
+    categories: ICategory[],
+    banners: IBanner,
+    adminSettingsResponse: IAdminSettingsResponse
+}
